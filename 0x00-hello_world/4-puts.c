61b098df5e35242f022d5b58dbab4de2b9146c66
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-02-16 15:56:31 -0800

    4-puts.c 8

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * mnain - this is a main function
+ * main - this is a main function
  *
  * Return: Always 0
  */
@@ -9,5 +9,6 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
+
