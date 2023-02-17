1b8d57e9dc8dba49fca9a4b32e26814b9f9dd21a
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-02-16 16:05:34 -0800

    5-printf.c 1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.sh>
+
+/**
+ * main - this is a main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
