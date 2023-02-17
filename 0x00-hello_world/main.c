267cabe1e39583e8c1631b29d720b162b420cf78
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-02-17 05:07:04 -0800

    100-intel 4

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this ia a main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("printing a string on the standard output.");
+	return (0);
+}
