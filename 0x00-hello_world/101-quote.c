9f9fccfa7da5c597ad07fdacb0eab75b986e29f4
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-02-17 08:37:44 -0800

    101-quote.c 3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
-
 /**
  * main - Entry point
+ *
  * Return: Always 1 (success)
  */
+
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	write(2, msg, sizeof(msg) - 1);
 	return (1);
 }
