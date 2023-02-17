ddba18c60ee5ba488c96655680ff7b8f481335ba
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-02-17 08:12:52 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,7 +6,8 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, sizeof(msg) - 1);
 	return (1);
 }
+
