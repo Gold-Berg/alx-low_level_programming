11bd2633f7f057bc3f68724f23b9e309c1e77d38
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-02-17 19:45:49 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
