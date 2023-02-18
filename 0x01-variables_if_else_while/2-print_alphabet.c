06cd5a7d5fe9faf351f80d23f96a82af45955ccd
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-02-17 18:02:01 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is a main function
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
+
