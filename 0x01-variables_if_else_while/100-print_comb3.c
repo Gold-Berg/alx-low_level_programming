ac9f82e69d0971f86a235c58f20903e933d3d1e3
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-02-19 15:56:39 -0800

    100-print_comb3 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,19 @@ int main(void)
 {
 	int i, j;
 	{
-		for (i = 0; i < 9; i++)
+		for (i = 0; i < 10; i++)
 		{
-			for (j = 0; j <= 9; j++)
+			for (j = 1; j < 10; j++)
 			{
-				if (j != i)
+				if (i < j && j != i)
 				{
-				putchar(i + '0');
-				putchar(j + '0');
-				}
-				if (i != 8 || j != 9)
-				{
-				putchar(',');
-				putchar(' ');
+					putchar(i + '0');
+					putchar(j + '0');
+					if (i + j != 17)
+					{
+					putchar(',');
+					putchar(' ');
+					}
 				}
 			}
 		}
