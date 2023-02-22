ccabba90e688d7c16564eee93f41f3334f4faa23
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-02-21 18:50:47 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * add - adds integers
+ * @a: int to add to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+
+}
