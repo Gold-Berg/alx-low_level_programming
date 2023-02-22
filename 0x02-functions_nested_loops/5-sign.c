da4b14bc08808c70af76ae36fc21da954fdffcbf
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-02-21 16:13:10 -0800

    task 5 and 4

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * print_sign - prints the sign
+ * @nL: the int to check
+ * Returni: 1 and prints + if greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints -if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
+}
