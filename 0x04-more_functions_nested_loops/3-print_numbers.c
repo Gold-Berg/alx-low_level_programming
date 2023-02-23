63034a35df90f22c889e0c34e75699611169f0ec
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-02-23 15:01:39 -0800

    3-print_numbers 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,13 +6,10 @@
  */
 void print_numbers(void)
 {
-	int i;
-	{
-
-	if (i == 0 && i < 10; i++)
-	{
-		_putchar(i + '0');
-	}
-	}
-	_putchar('\n');
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
 }
