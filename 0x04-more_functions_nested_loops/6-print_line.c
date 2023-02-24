3e2073dce542e871f52b28b9fef5f93c11aec9ca
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-02-23 16:15:22 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,18 @@
 /**
  * print_line - draws a straight line
  * @n: the number of times the character _ should be printed
- */
+*/
 void print_line(int n)
 {
-if (n <= 0) {
+int i;
+
+if (n <= 0)
+{
 putchar('\n');
 return;
 }
-for (int i = 0; i < n; i++) {
+for (i = 0; i < n; i++)
+{
 putchar('_');
 }
 putchar('\n');
