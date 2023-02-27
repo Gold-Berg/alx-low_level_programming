90ac8a7cb1c3e7ff67e559370d626d819e2f38bf
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-02-27 13:59:06 -0800

    Task 1 and 2

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include <stdio.h> 
+/**
+ * reset_to_98 - updates pointer value to 98
+ * Return: nothing
+ * @n: integer input pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+All 0x05-pointers_arrays_strings task
