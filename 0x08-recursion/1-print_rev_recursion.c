f0895eb85bdcb0df2a7efe6ab19418e706456251
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-03-08 15:22:56 -0800

    3-factorial

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/*
- * _print_rev_recursion - prints a string in reverse.
+/**
+ * _print_rev_recursion - Prints a string in reverse
  * @s: The string to print
- * Return: Nothing
+ * Return:
  */
 void _print_rev_recursion(char *s)
 {
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - Returns factorial of given number
+ * @n: The given number
+ * Return: Nothing
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (21);
+	return (n * factorial(n - 1));
+}
