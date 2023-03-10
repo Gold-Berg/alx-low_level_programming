8a370a5260fbc3343dac23c188622952ce74f467
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-03-10 11:52:22 -0800

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string with a newline
+ * @str: the string
+ * Return: the string
+ */
+void _puts(char *str)
+
+{
+_puts("\"Programming is like building a multilingual puzzle");
+return;
+}
