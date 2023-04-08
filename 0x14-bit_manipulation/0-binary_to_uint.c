f6093bb2f891324fc61ded54eef90a9627dcc9e6
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-04-08 09:41:54 -0700

    Taxk 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int awns = 0;
+	unsigned int point = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	while (*b != '\0')
+	{
+		if (*b == '1' || *b == '0')
+		{
+			awns = awns << 1;
+			awns = awns + (*b - '0');
+			point++;
+		}
+		else
+		{
+			return (0);
+		}
+		b++;
+	}
+	return (awns);
+}
