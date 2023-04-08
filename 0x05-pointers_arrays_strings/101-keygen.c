a269ecf73164f2d80ed8aef754f1ba673a2efd74
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-04-08 09:32:09 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random passwords
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char password[64];
+	int in = 0, sum = 0, diff1, diff2;
+
+	strand(time(0));
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++]
+
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alx-low_level_programming
+All ALx low level programming task
