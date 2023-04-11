8afe791ed678fb1413e69189fa690cb43a8e05ef
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-04-10 19:35:18 -0700

    Task 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include "main.h"
 #include <unistd.h>
 /**
  * create_file - Creates a file.
@@ -8,17 +9,19 @@
  * Return: If the function fails - -1.
  *         Otherwise - 1.
 */
-int create_file(const char *filename, char *text_content) {
+int create_file(const char *filename, char *text_content)
+{
 if (filename == NULL)
-return -1;
+return (-1);
 int thefile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (thefile == -1)
-return -1;
-if (text_content != NULL) {
+return (-1);
+if (text_content != NULL)
+{
 ssize_t bytes_written = write(fd, text_content, strlen(text_content));
 if (bytes_written == -1)
-return -1;
+return (-1);
 }
 close(thefile);
-return 1;
+return (1);
 }
