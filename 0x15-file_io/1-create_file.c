f72333b80e1c999af84b5209522624824324568e
Gold_Berg <igbinobaeseosa0@gmail.com>
2023-04-10 19:39:57 -0700

    Task 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
-#include <fcntl.h>
 #include "main.h"
+#include <string.h>
 #include <unistd.h>
 /**
  * create_file - Creates a file.
@@ -11,15 +11,17 @@
 */
 int create_file(const char *filename, char *text_content)
 {
+int thefile;
+ssize_t bytes;
 if (filename == NULL)
 return (-1);
-int thefile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+thefile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (thefile == -1)
 return (-1);
 if (text_content != NULL)
 {
-ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-if (bytes_written == -1)
+bytes = write(thefile, text_content, strlen(text_content));
+if (bytes == -1)
 return (-1);
 }
 close(thefile);
