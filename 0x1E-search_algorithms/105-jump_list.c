a011188ed17e06a77c16c4768a3c99e5b8c9dc4a
Gold-Berg <igbinobaeseosa0@gmail.com>
2024-07-17 15:53:29 -0700

    Task 12.2

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,6 +1,14 @@
 #include "search_algos.h"
 #include <math.h>
 
+/**
+ * jump_list - Searches for a value in a sorted singly linked list using Jump search algorithm.
+ * @list: Pointer to the head of the list to search in.
+ * @size: Number of nodes in the list.
+ * @value: Value to search for.
+ *
+ * Return: Pointer to the first node where value is located, or NULL if value is not present or list is NULL.
+ */
 
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
